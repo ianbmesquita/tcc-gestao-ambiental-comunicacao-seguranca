d9af10b314a36edbca90bb283b6ccf7d966f1aa9
Ian Mesquita <ianmesquitadb@gmail.com>
2023-09-26 14:22:18 -0300

    feat: Criando módulo de comunicação e segurança para envio de emails.

---
+++ src/consumer.js
@@ -0,0 +1,42 @@
+require ('dotenv').config();
+
+const amqp = require('amqplib');
+
+const { consultarAPI } = require('./residents')
+const { sendEmail } = require('./mailer')
+
+const rabbitMqConfig = {
+    host: process.env.HOST_QUEUE,
+    port: 5672,
+    username: process.env.USERNAME_QUEUE,
+    password: process.env.PASSWORD_QUEUE
+  }
+
+async function startConsumer() {
+    const connection = await amqp.connect(rabbitMqConfig);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue('sigam-notificacao-incidentes');
+
+    channel.consume('sigam-notificacao-incidentes', async (message) => {
+        try {
+            const data = JSON.parse(message.content.toString());
+            console.log("Dados: ", data);
+            const residents = await consultarAPI(data.idBarragem);
+            console.log("Residentes: ", residents);
+
+            residents.forEach(resident => {
+                sendEmail(data, resident.email);
+            });
+
+            channel.ack(message);
+        } catch (error) {
+            console.error("Erro ao processar a mensagem:", error);
+
+            channel.reject(message, false);
+        }
+        
+    });
+}
+
+module.exports = { startConsumer };
